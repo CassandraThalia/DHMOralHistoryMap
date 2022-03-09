37c841a1d5da4566320ebff7982e692a382dcb9e
CassandraThalia <71269821+CassandraThalia@users.noreply.github.com>
2022-03-08 20:03:55 -0400

    changed layout, added background texture

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (() => {
-    var map = L.map('map').setView([44.664376644077386, -63.576382637367736], 15);
+    var map = L.map('map').setView([44.664376644077386, -63.576382637367736], 14);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
