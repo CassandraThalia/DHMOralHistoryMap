6421df1c8dc35f88910ff3c333c95104b5bc924f
CassandraThalia <71269821+CassandraThalia@users.noreply.github.com>
2022-03-09 21:14:16 -0400

    removed description from popup, revert to here

--- main.js
+++ main.js
@@ -50,7 +50,7 @@
     var expIcon = new L.Icon({
         iconUrl: 'images/exp2.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [60, 60],
+        iconSize: [80, 80],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
@@ -66,62 +66,56 @@
     //----------------------------------- LISTENING MARKERS AND POPUPS ----------------------------------------- 
     let markerGroup = L.featureGroup().addTo(map);
 
-    let popUpSetUp = (title, desc, img, media) => {
+    let popUpSetUp = (title, img, media) => {
         return '<h1>' + title + '</h1> \
             <div id="cont"> \
                 <img src="' + img + '"/> \
-                <p>' + desc + '</p> \
                 <div id="player">' + createMediaPlayer(media) + '</div> \
             </div>'
     }
 
-    let popUpStyle = { className: "popUpStyle", maxWidth: "fit-content" };
+    let popUpStyle = { className: "popUpStyle", maxWidth: 400 };
 
     let langilleMarker = L.marker([44.66964336000262, -63.55930055991591], {icon: violetIcon})
     .bindPopup(popUpSetUp("Walter Langille Interview",
-        "Walden was at school at the time of the explosion. He recounts his memories of Dartmouth at the time, the aftermath of the blast, and the effect of the explosion on the many different Dartmouth industries.",
         "",
         "audio/Langille.mp3"), popUpStyle)
     .addTo(markerGroup);
 
+    let gentlesImg = "images/Gentles.png";
+
     let gentlesMarker = L.marker([44.658917, -63.571400], {icon: violetIcon})
     .bindPopup(popUpSetUp("Verna Gentles Interview",
-        "Verna Gentles was on the ferry at the time of the explosion. She was injured in the blast and was given medical attention from an officer and a nurse at a house on Victoria Road in Halifax.",
-        "images/Gentles.png",
+        gentlesImg,
         "audio/Gentles.mp3"), popUpStyle)
     .addTo(markerGroup);
 
     let ardleyMarker = L.marker([44.6693319810874, -63.56489863108046], {icon: violetIcon})
     .bindPopup(popUpSetUp("Mabel Ardley Interview",
-        "Mabel Ardley was only 8 years old at the time of the explosion. She was at school at Greenvale School on Ochterloney St. when the blast occurred.",
         "",
         "audio/Ardley.mp3"), popUpStyle)
     .addTo(markerGroup);
 
     let morashMarker = L.marker([44.67163177762183, -63.584244984874076], {icon: redIcon})
     .bindPopup(popUpSetUp("Ethel Morash Interview",
-        "Ethel Morash was at home in Dartmouth at the time of the explosion. She recalls her fortune in the event itself, as well as the strange circumstances she was discovered in after the blast. ",
         "",
         "audio/Morash.mp3"), popUpStyle)
     .addTo(markerGroup);
 
     let dillmanMarker = L.marker([44.671336002053174, -63.58072735706985], {icon: redIcon})
     .bindPopup(popUpSetUp("Leighton Dillman Interview",
-        "Leighton Dillman, namesake of the Dartmouth commons, was approximately 20 years old at the time of the explosion. He was working at the Consumer Cordage Rope Company when the blast occurred.",
         "images/Dillman.png",
         "audio/Dillman.mp3"), popUpStyle)
     .addTo(markerGroup);
 
     let robinsonMarker = L.marker([44.665919, -63.570708], {icon: redIcon})
     .bindPopup(popUpSetUp("Charles Robinson Interview",
-        "Mr. Robinson was home at the time of the blast but his sister was struck by debris and hurt on her walk to school. He also believes his dog had a premonition of the event.",
         "",
         "audio/Robinsons.mp3"), popUpStyle)
     .addTo(markerGroup);
 
     let campbellMarker = L.marker([44.64575930956331, -63.57620007341009], {icon: violetIcon})
     .bindPopup(popUpSetUp("Campbell Sisters Interview", 
-        "Frances Mary (Mollie) Campbell and Margaret Isobell Campbell (both nee Kuhn) of Dartmouth recount their memories of the day of the explosion, sometimes disagreeing on the order of events. The two sisters were at school at the Halifax County Academy at the time of the explosion.", 
         "", 
         ""), popUpStyle)
     .addTo(markerGroup)
@@ -160,15 +154,18 @@
         fillOpacity: 0.5,
         radius: 3500,
     });
+    let blastRadBtnTxt = document.querySelector("#radBtn");
     let blastRadBool = false
     function showBlastRad() { 
         if (blastRadBool === false){
             blastRadius.addTo(map);
             blastRadBool = true;
+            blastRadBtnTxt.innerText = "HIDE BLAST RADIUS"
         }
         else if (blastRadBool === true){
             map.removeLayer(blastRadius);
             blastRadBool = false;
+            blastRadBtnTxt.innerText = "SHOW BLAST RADIUS"
         }
     }
     const radBtn = document.querySelector("#radBtn");
